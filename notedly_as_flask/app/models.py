032b783b0405582822088e06b85d06ed2606d9aa
nh3223 <31948410+nh3223@users.noreply.github.com>
2021-02-05 10:44:49 -0800

    Added basic models

---
+++ notedly_as_flask/app/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from flask import current_app, request, url_for
+from flask_login import UserMixin
+
+from app.exceptions import ValidationError
+from . import db, login_manager
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(64))
+    notes = db.relationship('Note', backref='author', lazy='dynamic')
+
+class Note(db.Model):
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
