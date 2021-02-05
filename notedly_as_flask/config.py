d66a88a968871be816a7c6b3669bd1b9f83115af
nh3223 <31948410+nh3223@users.noreply.github.com>
2021-02-05 09:42:28 -0800

    Added initial config and app initialization files.

---
+++ notedly_as_flask/config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_TRACT_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
