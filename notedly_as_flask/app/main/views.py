99f9117421e1c123bc201fdfde4f90a61a45c670
nh3223 <31948410+nh3223@users.noreply.github.com>
2021-03-02 15:38:09 -0800

    Added functionality for showing all notes and writing new notes

--- notedly_as_flask/app/main/views.py
+++ notedly_as_flask/app/main/views.py
@@ -2,16 +2,21 @@ from flask import render_template, redirect, url_for, abort, flash, request, cur
 from flask_login import login_required, current_user
 
 from . import main
+from .. import db
 from .forms import NoteForm
 from ..models import User, Note
 
 @main.route('/', methods=['GET','POST'])
 def index():
+    notes = Note.query.order_by(Note.timestamp.desc())
+    return render_template('index.html', notes=notes)
+
+@main.route('/new', methods=['GET','POST'])
+def new():
     form = NoteForm()
     if form.validate_on_submit():
         note = Note(content = form.content.data, author = current_user._get_current_object())
         db.session.add(note)
         db.session.commit()
         return redirect(url_for('.index'))
-    notes = Note.query.order_by(Note.timestamp.desc())
-    return render_template('index.html', form=form, notes=notes)
+    return render_template('new.html', form=form)
