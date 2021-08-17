ff79e841ffac8bacbc6682777e452e6e0fdc37a7
nh3223 <31948410+nh3223@users.noreply.github.com>
2021-08-17 08:27:09 -0700

    Refactored CompanyName tests

--- 280G/280g-react-node/client/src/components/Screens/CompanyInformation/CompanyName/CompanyName.test.js
+++ 280G/280g-react-node/client/src/components/Screens/CompanyInformation/CompanyName/CompanyName.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect'
-import {render, screen, waitFor} from '@testing-library/react'
+import {act, render, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { RecoilRoot } from 'recoil';
 
@@ -8,90 +8,63 @@ import CompanyName from './CompanyName';
 import { companyNameState } from '../../../../recoil/company';
 import * as editCompany from '../../../../api/company/editCompany';
 
-test('should render form if no company name is provided', () => {
+const component = (companyName) => (
+  <RecoilRoot initializeState={ ({ set }) => set(companyNameState, companyName) }>
+    <CompanyName />
+  </RecoilRoot>
+);
 
-  const companyName = '';
+const defaultCompanyName = '';
+const givenCompanyName = 'test';
+const descriptionText = 'Company Name:';
 
-  render(
-    <RecoilRoot initializeState={ ({ set }) => set(companyNameState, companyName) }>
-      <CompanyName />
-    </RecoilRoot>
-  );
 
-  expect(screen.getByRole('textbox')).toBeInTheDocument();
-  
+test('should render description and form if no company name is provided', () => {
+  const { getByRole, getByText } = render(component(defaultCompanyName)); 
+  const description = getByText(descriptionText);
+  expect(description).toBeInTheDocument();
+  const input = getByRole('textbox');
+  expect(input).toBeInTheDocument(); 
 });
+
 
 test('should render description if company name is provided', () => {
-  
-  const companyName = 'test';
-
-  render(
-    <RecoilRoot initializeState={ ({ set }) => set(companyNameState, companyName) }>
-      <CompanyName />
-    </RecoilRoot>
-  );
-
-  expect(screen.getByText(companyName)).toBeInTheDocument();
-
+  const { getByText } = render(component(givenCompanyName));  
+  const description = getByText(descriptionText);
+  expect(description).toBeInTheDocument();
+  const companyName = getByText(givenCompanyName);
+  expect(companyName).toBeInTheDocument();
 });
 
-test('should call handleEdit and render form if Edit button is pressed', () => {
-  
-  const companyName = 'test'
 
-  render(
-    <RecoilRoot initializeState={ ({ set }) => set(companyNameState, companyName) }>
-      <CompanyName />
-    </RecoilRoot>
-  );
-
-  const editButton = screen.getByText('Edit');
-  
-  expect(editButton).toBeInTheDocument();
-  userEvent.click(editButton);
-  expect(screen.getByRole('textbox')).toBeInTheDocument();
-
+test('should render form if Edit button is pressed', () => {
+  const { getByText, getByRole } = render(component(givenCompanyName));  
+  const editButton = getByText('Edit');
+  expect(editButton).toBeInTheDocument();  
+  userEvent.click(editButton);  
+  const input = getByRole('textbox');
+  expect(input).toBeInTheDocument();
 });
 
-test('should call handleChange and show value in form if user types in form', () => {
-  
-  const companyName = ''
 
-  render(
-    <RecoilRoot initializeState={ ({ set }) => set(companyNameState, companyName) }>
-      <CompanyName />
-    </RecoilRoot>
-  );
- 
-  const input = screen.getByRole('textbox');
-  userEvent.type(input, 'test');
-  
-  expect(input).toHaveValue('test');
-
+test('should show value in form if user types in form', () => {
+  const { getByRole } = render(component(defaultCompanyName));
+  const input = getByRole('textbox');
+  userEvent.type(input, givenCompanyName);
+  expect(input).toHaveValue(givenCompanyName);
 });
 
-test('should call handleSubmit, should call editCompany, and render description after submit', async () => {
 
-  const companyName = '';
+test('should render description after submit', async () => {
   
-  const fakeEditCompanyResponse = 'test';
-
   jest.spyOn(editCompany, 'editCompany').mockImplementationOnce(() => Promise.resolve({
-    json: () => Promise.resolve(fakeEditCompanyResponse),
+    json: () => Promise.resolve({ companyName: givenCompanyName }),
   }));
-  
-  render(
-    <RecoilRoot initializeState={ ({ set }) => set(companyNameState, companyName) }>
-      <CompanyName />
-    </RecoilRoot>
-  );
 
-  const input = screen.getByRole('textbox');
-  userEvent.type(input, 'test');
-  
-  userEvent.type(input, '{enter}');
-
-  await waitFor(() => expect(screen.getByText('test')).toBeInTheDocument());
-
+  const { getByRole, getByText } = render(component(defaultCompanyName));
+  const input = getByRole('textbox');
+  userEvent.type(input, givenCompanyName);
+  await act(() => userEvent.type(input, '{enter}'));
+  const companyName = getByText(givenCompanyName);
+  await waitFor(() => expect(companyName).toBeInTheDocument());
 });
